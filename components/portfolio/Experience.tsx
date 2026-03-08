904bd6567b5cebedfbecf86268f9b70b67bce0ab
Hoàn Tiny <thedevil0903@gmail.com>
2026-03-08 19:22:52 +0700

    refactor: remove unused Image import in Experience component

--- components/portfolio/Experience.tsx
+++ components/portfolio/Experience.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useRef, useState } from 'react'
-import Image from 'next/image'
 import { AnimatePresence, motion, useInView } from 'framer-motion'
 
 export default function Experience() {
